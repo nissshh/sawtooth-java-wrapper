0e4d34a407744910e30477f9468f71be8bba248a
nirmalsandip <nirmalsandipm@gmail.com>
2018-04-10 21:58:59 +0530

    Adding unit tests

--- sawtooth-java-wrapper-client/src/test/java/com/mycompany/blockchain/sawtooth/client/asset/WalletPayloadClientTest.java
+++ sawtooth-java-wrapper-client/src/test/java/com/mycompany/blockchain/sawtooth/client/asset/WalletPayloadClientTest.java
@@ -15,6 +15,7 @@ import com.mycompany.blockchain.sawtooth.client.wallet.WalletPayloadClientServic
 import com.mycompany.blockchain.sawtooth.core.service.wallet.WalletAddressBuilder;
 import com.mycompany.blockchain.sawtooth.wallet.protobuf.SawtoothWalletPayload;
 import com.mycompany.blockchain.sawtooth.wallet.protobuf.SawtoothWalletPayload.CreateWallet;
+import com.mycompany.blockchain.sawtooth.wallet.protobuf.SawtoothWalletPayload.Deposit;
 import com.mycompany.blockchain.sawtooth.wallet.protobuf.Wallet;
 
 public class WalletPayloadClientTest extends BaseClientTest {
@@ -33,9 +34,8 @@ public class WalletPayloadClientTest extends BaseClientTest {
 
 	@Test
 	public void testCreateWallet() throws Exception {
-		String assetName = "Computer";
-
-		CreateWallet createWallet = CreateWallet.newBuilder().setCustomerId("001")
+		String customerId = "1151";
+		CreateWallet createWallet = CreateWallet.newBuilder().setCustomerId(customerId)
 				.setInitialBalance(5000).build();
 
 		SawtoothWalletPayload payload = SawtoothWalletPayload.newBuilder()
@@ -44,29 +44,36 @@ public class WalletPayloadClientTest extends BaseClientTest {
 		logger.info("Sending Payload as " + payload);
 		String resposne = service.submitStateChange(payload);
 		logger.info("State Change responsed from client Service : " + resposne);
-		Wallet wallet = Wallet.getDefaultInstance().newBuilder().setCustomerId("001").build();
+		Wallet wallet = Wallet.getDefaultInstance().newBuilder().setCustomerId(customerId).build();
 		String address = new WalletAddressBuilder("wallet", "1.0").buildAddress(wallet);
 		System.out.println("Getting data for at address   :    " + address);
 		ByteString response = zmqTemplate.getClientGetStateRequest(address);
 		Wallet walletAtAddress = Wallet.parseFrom(response);
 		System.out.println("Asset found as " + walletAtAddress);
-		Assert.assertEquals("001", walletAtAddress.getCustomerId());
+		Assert.assertEquals(customerId, walletAtAddress.getCustomerId());
 
 	}
 
-	/*@Test
-	@Ignore
-	public void testUpdateAsset() throws Exception {
-		CreateAsset createAsset = CreateAsset.getDefaultInstance().newBuilder().setName("Mobile")
-				.setType("Electronix").setDetails("A Mobile used as test for Unit testing.")
-				.setValue(300).build();
-		UpdateAsset updateAsset = UpdateAsset.getDefaultInstance().newBuilder().setName("Mobile")
-				.setValue(500).build();
-		AssetPayload payload = AssetPayload.getDefaultInstance().newBuilder()
-				.setUdpateAsset(updateAsset).setPayloadType(PayloadType.UPDATE_ASSET).build();
+	@Test
+	public void testDepositWallet() throws Exception {
+		String customerId = "1151";
+		Deposit depositWallet = Deposit.newBuilder().setCustomerId(customerId)
+				.setAmount(5000).build();
+
+		SawtoothWalletPayload payload = SawtoothWalletPayload.newBuilder()
+				.setDeposit(depositWallet).build();
+
 		logger.info("Sending Payload as " + payload);
 		String resposne = service.submitStateChange(payload);
-		logger.info("Response from client Service : " + resposne);
-	}*/
+		logger.info("State Change responsed from client Service : " + resposne);
+		Wallet wallet = Wallet.getDefaultInstance().newBuilder().setCustomerId(customerId).build();
+		String address = new WalletAddressBuilder("wallet", "1.0").buildAddress(wallet);
+		System.out.println("Getting data for at address   :    " + address);
+		ByteString response = zmqTemplate.getClientGetStateRequest(address);
+		Wallet walletAtAddress = Wallet.parseFrom(response);
+		System.out.println("Asset found as " + walletAtAddress);
+		Assert.assertEquals(customerId, walletAtAddress.getCustomerId());
+
+	}
 
 }
